6531403676ddf84210fd6a4597a90a9acdc087b7
P-Pratik <114237737+P-Pratik@users.noreply.github.com>
2024-06-15 23:10:22 +0530

    fixed security issues

--- routes.py
+++ routes.py
@@ -89,7 +89,7 @@ def register_routes(app, db, bcrypt):
 
         elif request.method == "POST":
             return
-        
+
     @app.route("/contest", methods=["GET"])
     def contest():
         return render_template("contest.html")
@@ -110,17 +110,14 @@ def register_routes(app, db, bcrypt):
         lcContest = lc.getUpcomingContest()
         return jsonify({"gfgcontest": gfgContest, "lccontest": lcContest})
 
-
     @app.route("/api/contest/past/<page>", methods=["POST"])
-    def getPastContest(page = 1):
+    def getPastContest(page=1):
         if request.json:
             platform = request.json["platform"]
 
-        print(platform)
         gfgContest = gfg.getPastContest(int(page))
         lcContest = lc.getPastContest(int(page))
         return jsonify({"gfgcontest": gfgContest, "lccontest": lcContest})
-
 
     @app.route("/update/profile", methods=["PUT"])
     @login_required
@@ -150,7 +147,8 @@ def register_routes(app, db, bcrypt):
                 return jsonify(success=True)
             except Exception as e:
                 db.session.rollback()
-                return jsonify(success=False, error=str(e))
+                print(e)
+                return jsonify(success=False, error="Some error occurred")
         else:
             return jsonify(success=False, error="User not found")
 
@@ -216,7 +214,7 @@ def register_routes(app, db, bcrypt):
             return jsonify(success=True)
         except Exception as e:
             db.session.rollback()
-            return jsonify(success=False, error=str(e))
+            return jsonify(success=False, error="Some error occurred")
 
     @app.route("/check/username-exists", methods=["POST"])
     @login_required
@@ -224,24 +222,20 @@ def register_routes(app, db, bcrypt):
         if not request.json:
             return jsonify(success=False, error="No data provided")
 
-        print(request.json)
         platform = request.json["platform"]
         username = request.json["username"]
 
-        print(platform, username)
         if platform == "geeksforgeeks":
             data = gfg.usernameExists(username)
             if "error" in data:
                 return jsonify(exists=False, error=data["error"])
-            
+
         elif platform == "leetcode":
             data = lc.usernameExists(username)
             if "error" in data:
                 return jsonify(exists=False, error=data["error"])
-        
+
         return jsonify(exists=data["exists"])
-
-
 
     @app.route("/problem/<page>", methods=["POST"])
     def problem(page):
